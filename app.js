af8a40f6ff9c52cd760e112ed3d782b1dd3a1b2e
IssaSalman <>
2021-06-16 12:51:54 +0000

    added new files

---
+++ app.js
@@ -0,0 +1,28 @@
+var name = prompt("what is your name?")
+
+alert("welcome to Nvidia RTX website " + name)
+
+var favcard = prompt("what is your fav card from these (3070, 3080  , 3090 )?")
+
+
+if(favcard == "3070"){
+  document.write("<p>"+ "great thing you choosed RTX 3070 you will have fun using it"+"</p>")
+}
+ else if(favcard == "3080")
+{
+ document.write("<p>"+ "great thing you choosed RTX 3080 you will have fun using it"+"</p>")
+
+
+
+}
+
+else if(favcard == "3090")
+{
+ document.write("<p>"+ "great thing you choosed RTX 3090 you will have fun using it"+"</p>")
+}
+else{
+  alert("We are  sorry that  we don't have this Model maybe in the future ^^ thanks for your visit")
+}
+
+
+
